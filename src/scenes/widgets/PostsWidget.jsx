47f729c1ea84728ecfb45024adece185be296aa9
Souvik Das <souvikdas201716@gmail.com>
2023-09-26 22:53:11 +0530

    FINAL COMMIT

---
+++ src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state"; // Update the import path to your setPosts action
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  const getPosts = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/posts", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      dispatch(setPosts({ posts: data })); // Dispatch the setPosts action here
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  const getUserPosts = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/posts/${userId}/posts`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      dispatch(setPosts({ posts: data })); // Dispatch the setPosts action here
+    } catch (error) {
+      console.error("Error fetching user posts:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, [isProfile, userId, dispatch, token]);
+
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
+};
+
+export default PostsWidget;
